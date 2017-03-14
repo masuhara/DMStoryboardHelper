a4d51d7a5c478ee1a044cbfdc8b18303019ef818
masuhara <info@masuhara.net>
2017-03-15 00:27:27 +0900

    Modify

--- StoryboardHelper.swift
+++ StoryboardHelper.swift
@@ -8,26 +8,39 @@
 
 import UIKit
 
-class StoryboardHelper: NSObject {
-    class func adjust() -> UIStoryboard {
+struct StoryboardHelper {
+    
+    static var currentDevice = UIDevice.current
+    static var height = UIScreen.main.bounds.size.height
+    
+    static func adjust(to window: UIWindow?) {
+        
+        // Check window
+        guard let window = window else {
+            print("Window is nil.")
+            return
+        }
+        
         var storyboard = UIStoryboard()
-        let height = UIScreen.main.bounds.size.height
         if height == 667 {
-            // iPhone6
+            // iPhone6,6s,7,7s
             storyboard = UIStoryboard(name: "Main", bundle: nil)
-        }else if height == 736 {
-            // iPhone6 Plus
+        } else if height == 736 {
+            // iPhone6+,7+
             storyboard = UIStoryboard(name: "Main", bundle: nil)
-        }else if height == 480 {
-            // iPhone4s
+        } else if height == 480 {
+            // iPhone4,4s
             storyboard = UIStoryboard(name: "3.5inch", bundle: nil)
-        }else if height == 1024 {
-            // iPad
+        } else if height == 1024 {
+            // iPad,2,Air,mini
             storyboard = UIStoryboard(name: "3.5inch", bundle: nil)
-        }else {
-            // iPhone5・5s・5c + 何も読み込めなかった場合
+        } else {
+            // iPhone5,5s,5c,unknown
             storyboard = UIStoryboard(name: "Main", bundle: nil)
         }
-        return storyboard
+        
+        window.rootViewController = storyboard.instantiateInitialViewController()! as UIViewController
+        window.makeKeyAndVisible()
     }
+    
 }
